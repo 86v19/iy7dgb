018af047c0025c95a9e9aaa33c13997ef84ec715
86v19 <155864121+86v19@users.noreply.github.com>
2024-01-09 08:23:58 -0800

    Update perfector.js

--- docs/drive/perfector.js
+++ docs/drive/perfector.js
@@ -2,17 +2,28 @@
  * Perfector
  * This code is released under the GNU General Public License version 2.
  */
-$(document).ready(function () {
-	$("#perfector").attr("placeholder", "Hello world.");
-}
-
 $(document).keyup(function(event) {
     if ($("#perfector").is(":focus") && event.key == "Enter") {
 	    try {
-		let perfector = new RiveScript();
+		rs = new RiveScript({
+				debug:   debugMode,
+				onDebug: onDebug
+			});
+		
+			// Load our files from the brain/ folder.
+			rs.loadFile([
+				"perfector/brain/begin.rive",
+				"perfector/brain/admin.rive",
+				"perfector/brain/clients.rive",
+				"perfector/brain/eliza.rive",
+				"perfector/brain/myself.rive",
+				"perfector/brain/rpg.rive",
+				"perfector/brain/javascript.rive"
+			]).then(onReady).catch(onError);
+		    
 	 	var message = document.getElementById("perfector").value;
 		document.getElementById("perfector").value = "";
-		document.getElementById("perfector").value = perfector(message);
+		document.getElementById("perfector").value = rs.(message);
 		} catch (error) {
 			document.getElementById("perfector").value = "Fyi: " + error;
 		setTimeout(() => {
